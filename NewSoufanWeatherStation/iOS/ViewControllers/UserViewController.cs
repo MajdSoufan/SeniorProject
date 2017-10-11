99bd3e4a9976c321d2248a216bc7727acce8bd90
MajdSoufan <ms585@evansville.edu>
2017-10-11 18:59:58 -0500

    Fixing the offset issue. Plus, animating the border changing function

--- NewSoufanWeatherStation/iOS/ViewControllers/UserViewController.cs
+++ NewSoufanWeatherStation/iOS/ViewControllers/UserViewController.cs
@@ -21,30 +21,51 @@ namespace NewSoufanWeatherStation.iOS
         {
             if (EditButton.TitleLabel.Text.Equals("Edit"))
             {
-                UpdateEditCancelButtonsStatus("Save", true, 1);
+                UpdateUIElements("Save", true, 1, UITextBorderStyle.RoundedRect, (float)-7.0);
             }
             else
             {
-                UpdateEditCancelButtonsStatus("Edit", false, 0);
+                UpdateUIElements("Edit", false, 0, UITextBorderStyle.None, (float)7.0);
+
             }
         }
 
         partial void CancelButton_TouchUpInside(UIButton sender)
         {
-            UpdateEditCancelButtonsStatus("Edit", false, 0);
+            UpdateUIElements("Edit", false, 0, UITextBorderStyle.None, (float)7.0);
         }
 
-        private void UpdateEditCancelButtonsStatus(String buttonText, bool buttonEnabled, float buttonAlpha)
+
+        private void UpdateUIElements(String buttonText, bool buttonEnabled, float buttonAlpha, UITextBorderStyle borderStyle, float offset)
         {
             EditButton.SetTitle(buttonText, UIControlState.Normal);
             CancelButton.Enabled = buttonEnabled;
 
+            FirstNameTextField.Enabled = buttonEnabled;
+            LastNameTextField.Enabled = buttonEnabled;
+            EmailTextField.Enabled = buttonEnabled;
+            PasswordTextField.Enabled = buttonEnabled;
+
             //Fade In/Out Animation
             double fadingDuration = 0.1;
-            UIView.Animate(fadingDuration, 0, UIViewAnimationOptions.CurveEaseIn, ()=>
+            UIView.Animate(fadingDuration, 0, UIViewAnimationOptions.CurveEaseIn, () =>
             {
                 CancelButton.Alpha = buttonAlpha;
-            },null);
+                FirstNameTextField.BorderStyle = borderStyle;
+                LastNameTextField.BorderStyle = borderStyle;
+                EmailTextField.BorderStyle = borderStyle;
+                PasswordTextField.BorderStyle = borderStyle;
+
+            }, null);
+
+            //Fixing the offset after changing the border style
+            FirstNameTextField.Frame = new CoreGraphics.CGRect(new CoreGraphics.CGPoint(FirstNameTextField.Frame.X + offset, FirstNameTextField.Frame.Y), FirstNameTextField.Frame.Size);
+            LastNameTextField.Frame = new CoreGraphics.CGRect(new CoreGraphics.CGPoint(LastNameTextField.Frame.X + offset, LastNameTextField.Frame.Y), LastNameTextField.Frame.Size);
+            EmailTextField.Frame = new CoreGraphics.CGRect(new CoreGraphics.CGPoint(EmailTextField.Frame.X + offset, EmailTextField.Frame.Y), EmailTextField.Frame.Size);
+            PasswordTextField.Frame = new CoreGraphics.CGRect(new CoreGraphics.CGPoint(PasswordTextField.Frame.X + offset, PasswordTextField.Frame.Y), PasswordTextField.Frame.Size);
+
         }
+
+      
     }
 }
